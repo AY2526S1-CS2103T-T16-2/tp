5a5a93557aa701f7e3102417d24bf13834d62041
liawjarell <jarellliaw@gmail.com>
2025-09-25 15:18:04 +0800

    Add JUnit test

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,10 +1,12 @@
 package seedu.address.logic.commands;
 
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
 public class RemarkCommand extends Command {
 
     public static final String COMMAND_WORD = "remark";
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command is not yet implemented.";
 
     @Override
     public CommandResult execute(Model model) {
