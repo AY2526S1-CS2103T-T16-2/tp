478028fcbacce5322fb7291fd7baf47f78be069a
aaroncjgoh <aarongohchongjun@gmail.com>
2025-10-29 22:37:01 +0800

    Email now requires period after domain

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -18,9 +18,9 @@ public class Messages {
             + " inprogress, successful, unsuccessful";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_MANY_PERSONS_FOUND_OVERVIEW =
-            "%1$d persons found matching your description!";
+            "%1$d persons found matching your description! Enter 'list' command to view all clients again!";
     public static final String MESSAGE_ONE_PERSON_FOUND_OVERVIEW =
-            "1 person found matching your description!";
+            "1 person found matching your description! Enter 'list' command to view all clients again!";
     public static final String MESSAGE_NO_PERSONS_FOUND_OVERVIEW =
             "No person was found matching your description!";
     public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified "
